066830c485e95cf9fd4034c3b95a88543760b174
susieqt <susieqt25@gmail.com>
2018-09-19 12:45:07 -0400

    removed the battle text grammar code as it's redundant in english (every battle phrase is identical in english, while japanese has 5 variations depending on the move used, all it does in english is waste rom space)

--- data/text/common_2.asm
+++ data/text/common_2.asm
@@ -720,11 +720,7 @@ UnknownText_0x1c0d6c::
 _ActorNameText::
 	text "<USER>@@"
 
-_UsedMove1Text::
-	text_start
-	line "used @@"
-
-_UsedMove2Text::
+_UsedMoveText::
 	text_start
 	line "used @@"
 
@@ -734,25 +730,6 @@ _UsedInsteadText::
 
 _MoveNameText::
 	text_from_ram wStringBuffer2
-	db "@@"
-
-_EndUsedMove1Text::
-	text "!"
-	done
-
-_EndUsedMove2Text::
-	text "!"
-	done
-
-_EndUsedMove3Text::
-	text "!"
-	done
-
-_EndUsedMove4Text::
-	text "!"
-	done
-
-_EndUsedMove5Text::
 	text "!"
 	done
 
