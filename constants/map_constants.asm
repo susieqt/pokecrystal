ea4f3f331a0c1b0b9cbe1c6702df085a40d88b7d
susieqt <susieqt25@gmail.com>
2018-09-21 16:38:49 -0400

    fixed a map thing, still no clue why it crashes when leaving your house though

--- constants/map_constants.asm
+++ constants/map_constants.asm
@@ -447,7 +447,7 @@ ENDM
 	newgroup                                                      ; 24
 
 	map_const ROUTE_26,                                    10, 54 ;  1
-	map_const ROUTE_27,                                    40,  9 ;  2
+	map_const ROUTE_27,                                    13, 13 ;  2
 	map_const ROUTE_29,                                    14, 12 ;  3
 	map_const NEW_BARK_TOWN,                               13, 12 ;  4
 	map_const ELMS_LAB,                                     5,  6 ;  5
@@ -459,6 +459,7 @@ ENDM
 	map_const DAY_OF_WEEK_SIBLINGS_HOUSE,                   4,  4 ; 11
 	map_const ROUTE_27_SANDSTORM_HOUSE,                     4,  4 ; 12
 	map_const ROUTE_29_ROUTE_46_GATE,                       5,  4 ; 13
+	map_const CHERRYGROVE_MART,                             6,  4 ; 14
 
 	newgroup                                                      ; 25
 
@@ -483,12 +484,11 @@ ENDM
 	map_const ROUTE_30,                                    10, 27 ;  1
 	map_const ROUTE_31,                                    20,  9 ;  2
 	map_const CHERRYGROVE_CITY,                            20,  9 ;  3
-	map_const CHERRYGROVE_MART,                             6,  4 ;  4
-	map_const CHERRYGROVE_POKECENTER_1F,                    5,  4 ;  5
-	map_const CHERRYGROVE_GYM_SPEECH_HOUSE,                 4,  4 ;  6
-	map_const GUIDE_GENTS_HOUSE,                            4,  4 ;  7
-	map_const CHERRYGROVE_EVOLUTION_SPEECH_HOUSE,           4,  4 ;  8
-	map_const ROUTE_30_BERRY_HOUSE,                         4,  4 ;  9
-	map_const MR_POKEMONS_HOUSE,                            4,  4 ; 10
-	map_const ROUTE_31_VIOLET_GATE,                         5,  4 ; 11
+	map_const CHERRYGROVE_POKECENTER_1F,                    5,  4 ;  4
+	map_const CHERRYGROVE_GYM_SPEECH_HOUSE,                 4,  4 ;  5
+	map_const GUIDE_GENTS_HOUSE,                            4,  4 ;  6
+	map_const CHERRYGROVE_EVOLUTION_SPEECH_HOUSE,           4,  4 ;  7
+	map_const ROUTE_30_BERRY_HOUSE,                         4,  4 ;  8
+	map_const MR_POKEMONS_HOUSE,                            4,  4 ;  9
+	map_const ROUTE_31_VIOLET_GATE,                         5,  4 ; 10
 	
